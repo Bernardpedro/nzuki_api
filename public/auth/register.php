16ea2beb1cd57f87b6277a31e03f8a771bcae235
Bernardpedro <bernardpedro80@gmail.com>
2025-12-29 14:03:06 +0300

    fix the successful message

--- public/auth/register.php
+++ public/auth/register.php
@@ -1,5 +1,16 @@
 <?php
 
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
+    http_response_code(200);
+    exit;
+}
+
 require __DIR__ . "/../../vendor/autoload.php";
 
 $conn = mysqli_connect("localhost", "root", "", "nzuki_db");
@@ -23,7 +34,7 @@ $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt, "sss", $name, $email, $pass);
 
 if (mysqli_stmt_execute($stmt)) {
-    echo json_encode(["success" => true, "message" => "User registered"]);
+    echo json_encode(["success" => true, "message" => "User registered successfully"]);
 } else {
     echo json_encode(["success" => false, "error" => mysqli_error($conn)]);
 }
